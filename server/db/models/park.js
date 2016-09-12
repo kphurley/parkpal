6c648563d996dd1b1e65df0cc46035f6d3700b70
dejongnj <dejongnj@gmail.com>
2016-09-12 15:30:30 -0500

    park model created

---
+++ server/db/models/park.js
@@ -0,0 +1,32 @@
+'use strict';
+var Sequelize = require('sequelize');
+
+var db = require('../db');
+
+var park = db.define('park', {
+	name: {
+		type: Sequelize.STRING,
+		allowNull: false
+	},
+	location: {
+		type: Sequelize.STRING,
+		allowNull: false
+	},
+	description: {
+		type: Sequelize.TEXT,
+		allowNull: false
+	},
+	email: {
+		type: Sequelize.STRING,
+		allowNull: false,
+		validate: {
+			isEmail: true
+		}
+	},
+	phone: {
+		type: Sequelize.STRING,
+		allowNull: false
+	}
+});
+
+module.exports = park;
