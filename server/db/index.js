99a9e5656fb300fc5985acba0a1f5179c05f004a
Kevin Hurley <kphurley@gmail.com>
2016-09-13 10:26:23 -0500

    associations complete, small fix on park model

--- server/db/index.js
+++ server/db/index.js
@@ -4,6 +4,33 @@ module.exports = db;
 
 // eslint-disable-next-line no-unused-vars
 var User = require('./models/user');
+var Park = require('./models/park');
+var Facility = require('./models/facility');
+var Slot = require('./models/slot');
+var ParkAccess = require('./models/parkAccess');
+var Type = require('./models/type');
+var Transaction = require('./models/transaction');
+var Cart = require('./models/cart');
 
 // if we had more models, we could associate them in this file
 // e.g. User.hasMany(Reports)
+Park.hasMany(Facility);
+
+Facility.hasMany(Slot);
+
+User.hasMany(Transaction);
+
+Transaction.hasMany(Slot);
+
+Cart.hasMany(Slot);
+
+User.belongsToMany(Park, {through: ParkAccess});
+Park.belongsToMany(User, {through: ParkAccess});
+
+Facility.belongsToMany(Type, {through: 'facility_type'});
+Type.belongsToMany(Facility, {through: 'facility_type'});
+
+
+Cart.belongsTo(User, {as: 'userId'});
+
+
