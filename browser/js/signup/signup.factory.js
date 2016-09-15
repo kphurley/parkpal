03f4438bc9735111b444732fa1fafee721568f17
dejongnj <dejongnj@gmail.com>
2016-09-15 15:32:48 -0500

    hacking login after signup

--- browser/js/signup/signup.factory.js
+++ browser/js/signup/signup.factory.js
@@ -3,9 +3,11 @@ app.factory('SignupFactory', function($http) {
 
 
 	SignupFactory.createUser = function (data) {
+	var tempPass = data.password;
     return $http.post('/api/users', data)
     .then(function (response) {
       var user = response.data;
+      user.password = tempPass;
       return user;
     });
   };
