58f36e98ee62be53270f5994b14f8b17ad86a04f
dejongnj <dejongnj@gmail.com>
2016-09-17 15:06:16 -0500

    landing, signup and login pages done

--- browser/js/home/home.controllers.js
+++ browser/js/home/home.controllers.js
@@ -1,9 +1,13 @@
 'use strict';
 
-app.controller('HomeCtrl', function($scope, HomeFactory) {
+app.controller('HomeCtrl', function($scope, HomeFactory, $state) {
 	HomeFactory.findAll()
 	.then(function(parks) {
 		$scope.parks = parks
 	});
+
+	$scope.goToPark = function(parkId)  {
+		$state.go('park', {id: parkId});
+	}
 
 });
