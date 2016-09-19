9f66fdda37e7a8a71d34aa5e58ebb12f3445a1ba
dejongnj <dejongnj@gmail.com>
2016-09-19 13:34:54 -0500

    admin route authentication

---
+++ server/app/routes/authAPI.js
@@ -0,0 +1,23 @@
+
+var authAPI = {};
+
+authAPI.isLoggedIn = function(req, res, next) {
+	if (!req.user) {
+		res.status(401).end();
+	}
+	next()
+}
+
+authAPI.isAdmin = function (req, res, next) {
+	if (!req.user) {
+		res.status(401).end()
+		}
+	if (!req.user.isAdmin) {
+		res.status(403).end();
+	}
+	next()
+}
+
+
+
+module.exports = authAPI;
