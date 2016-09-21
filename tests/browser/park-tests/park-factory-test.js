6c7263335e8c247c1393c933c10b14a7f091fae8
Kevin Hurley <kphurley@gmail.com>
2016-09-20 19:59:53 -0500

    browser test attempt

--- tests/browser/park-tests/park-factory-test.js
+++ tests/browser/park-tests/park-factory-test.js
@@ -5,33 +5,32 @@ describe('Park Factory', function () {
 
     var $httpBackend;
     beforeEach(inject(function (_$rootScope_, _ParkFactory_) {
-        $rootScope = _$rootScope_;
-        ParkFactory = _ParkFactory_;
+      $rootScope = _$rootScope_;
+      ParkFactory = _ParkFactory_;
     }));
-/*
 
     it('should be an object', function () {
-        expect(Session).to.be.an('object');
+      expect(ParkFactory).to.be.an('object');
     });
 
-    it('should by default have id and user as null', function () {
-        expect(Session.user).to.be.equal(null);
-        expect(Session.id).to.be.equal(null);
+
+    describe('Find all method', function () {
+
+      afterEach(function () {
+        $httpBackend.verifyNoOutstandingExpectation();
+        $httpBackend.verifyNoOutstandingRequest();
+      });
+
+      it('should make a request to GET /api/parks', function (done) {
+
+        $httpBackend.expectGET('/api/parks');
+        //$httpBackend.whenGET('/api/parks').respond({user: {}});
+        $httpBackend.flush();
+
+      });
+
     });
-
-    describe('create method', function () {
-
-        it('should when called with id and user arguments' +
-        'set the id and user to session', function () {
-            var id = 'testId';
-            var user = {};
-            Session.create(id, user);
-            expect(Session.user).to.be.equal(user);
-            expect(Session.id).to.be.equal(id);
-        });
-
-    });
-
+/*
     describe('destroy method', function () {
 
         it('should set user and id to null', function () {
@@ -46,32 +45,7 @@ describe('Park Factory', function () {
 
         });
 
-    });
-
-    describe('event listening', function () {
-
-        it('should call destroy when notAuthenticated event is fired', function () {
-
-            var spy = sinon.spy(Session, 'destroy');
-
-            $rootScope.$broadcast(AUTH_EVENTS.notAuthenticated);
-
-            expect(spy.called).to.be.ok;
-            spy.restore();
-
-        });
-
-        it('should call destroy when sessionTimeout event is fired', function () {
-
-            var spy = sinon.spy(Session, 'destroy');
-
-            $rootScope.$broadcast(AUTH_EVENTS.sessionTimeout);
-
-            expect(spy.called).to.be.ok;
-            spy.restore();
-
-        });
-
     });*/
+
 
 });
